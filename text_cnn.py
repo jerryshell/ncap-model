e939d9c443f404b748c9c3ae330020a2950fed46
Jerry <calljerryli@outlook.com>
2020-03-01 04:35:29 +0800

    新模型

--- text_cnn.py
+++ text_cnn.py
@@ -38,23 +38,33 @@ class TextCNN(keras.Model):
 
 
 def create_model(feature1_number, feature2_number):
-    # inputs = keras.Input(shape=(feature1_number, feature2_number))
-    #
-    # cnn1 = keras.layers.Conv1D(256, 3, padding='same', strides=1, activation='relu')(input)
-    # cnn1 = keras.layers.MaxPooling1D(pool_size=48)(cnn1)
-    #
-    # cnn2 = keras.layers.Conv1D(256, 4, padding='same', strides=1, activation='relu')(input)
-    # cnn2 = keras.layers.MaxPooling1D(pool_size=47)(cnn2)
-    #
-    # cnn3 = keras.layers.Conv1D(256, 5, padding='same', strides=1, activation='relu')(input)
-    # cnn3 = keras.layers.MaxPooling1D(pool_size=46)(cnn3)
-    #
-    # cnn = keras.layers.concatenate([cnn1, cnn2, cnn3], axis=-1)
-    # flat = keras.layers.Flatten()(cnn)
-    # drop = keras.layers.Dropout(0.2)(flat)
-    # outputs = keras.layers.Dense(2, activation='softmax')(drop)
+    inputs = keras.layers.Input(shape=(feature1_number, feature2_number))
 
-    inputs = keras.Input(shape=(feature1_number, feature2_number))
+    cnn1 = keras.layers.Conv1D(256, 3, padding='same', strides=1, activation='relu')(inputs)
+    cnn1 = keras.layers.MaxPooling1D(pool_size=48)(cnn1)
+
+    cnn2 = keras.layers.Conv1D(256, 4, padding='same', strides=1, activation='relu')(inputs)
+    cnn2 = keras.layers.MaxPooling1D(pool_size=47)(cnn2)
+
+    cnn3 = keras.layers.Conv1D(256, 5, padding='same', strides=1, activation='relu')(inputs)
+    cnn3 = keras.layers.MaxPooling1D(pool_size=46)(cnn3)
+
+    cnn = keras.layers.concatenate([cnn1, cnn2, cnn3], axis=-1)
+    flat = keras.layers.Flatten()(cnn)
+    drop = keras.layers.Dropout(0.2)(flat)
+    outputs = keras.layers.Dense(2, activation='softmax')(drop)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(
+        loss='sparse_categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
+    return model
+
+
+def create_model2(feature1_number, feature2_number):
+    inputs = keras.layers.Input(shape=(feature1_number, feature2_number))
     flat = keras.layers.Flatten()(inputs)
     x = keras.layers.Dense(64, activation='relu')(flat)
     x = keras.layers.Dense(64, activation='relu')(x)
