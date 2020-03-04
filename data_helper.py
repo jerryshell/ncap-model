e5c8225ed5fdc1f6c345ca97ecea1d6579e92262
Jerry <calljerryli@outlook.com>
2020-03-04 16:32:50 +0800

    从最后一个词开始填充 vector

--- data_helper.py
+++ data_helper.py
@@ -20,19 +20,22 @@ class DataHelper:
         batch_label = np.zeros(shape=(batch_size,))
         batch_data = np.zeros(shape=(batch_size, self.feature1_number, self.feature2_number))
         for batch_index in range(batch_size):
-            label, data = data_loader.next()
-            # print(label, data)
-            word_list = data.split(' ')
+            label, sentence = data_loader.next()
+            # print(label, sentence)
+            word_list = sentence.split(' ')
+            word_list_len = len(word_list)
             # print(word_list)
-            # print(len(word_list))
+            # print(word_list_len)
+            # 从最后一个词开始填充 vector
             vector = np.zeros(shape=(self.feature1_number, self.feature2_number))
             for i in range(self.feature1_number):
-                if i < len(word_list):
-                    word = word_list[i]
+                if i < word_list_len:
+                    word = word_list[word_list_len - 1 - i]
                     vec = self.word2vec(word)
-                    vector[i] = vec
+                    vector[self.feature1_number - 1 - i] = vec
             batch_label[batch_index] = label
             batch_data[batch_index] = vector
+            # print(vector)
         return batch_label, batch_data
 
     # 将一句话转成向量
@@ -40,16 +43,19 @@ class DataHelper:
         # 去特殊字符
         sentence = re.sub(r'\W+', ' ', sentence).replace('_', ' ')
         # 分词
-        cut = jieba.lcut(sentence)
+        word_list = jieba.lcut(sentence)
         # 去空格
-        cut = [item for item in cut if item != ' ']
-        print(cut)
+        word_list = [item for item in word_list if item != ' ']
+        word_list_len = len(word_list)
+        print(word_list)
+        # print(word_list_len)
         vector = np.zeros(shape=(self.feature1_number, self.feature2_number))
         for i in range(self.feature1_number):
-            if i < len(cut):
-                word = cut[i]
+            if i < word_list_len:
+                word = word_list[word_list_len - 1 - i]
                 vec = self.word2vec(word)
-                vector[i] = vec
+                vector[self.feature1_number - 1 - i] = vec
+        # print(vector)
         return vector
 
     # 将一句话转成测试数据
@@ -61,9 +67,11 @@ class DataHelper:
 
 if __name__ == '__main__':
     from data_loader import DataLoader
+    import sys
 
     feature1_number = 60
     feature2_number = 300
     data_loader = DataLoader()
     data_helper = DataHelper(feature1_number, feature2_number)
-    print(data_helper.get_batch_label_and_vector(data_loader, 10))
+    np.set_printoptions(threshold=sys.maxsize)  # 强制打印 numpy 的整个数组
+    print(data_helper.get_batch_label_and_vector(data_loader, 100))
