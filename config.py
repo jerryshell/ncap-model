478f526e9c26688c6941aac7fc9a06661530efe2
Jerry <calljerryli@outlook.com>
2020-04-23 00:01:38 +0800

    调整 cnn 参数

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # 超参数
-feature1_number = 60  # 句子分成多少个词语，多余截断，不够补 0
+feature1_number = 64  # 句子分成多少个词语，多余截断，不够补 0
 feature2_number = 300  # 每个词语的向量
--- train_function.py
+++ train_function.py
@@ -14,13 +14,13 @@ model = create_model_text_cnn_separable(config.feature1_number, config.feature2_
 
 # 加载数据
 epoch = 10
-batch_size = 500
+batch_size = 64
 num_data = data_loader.num_data
 num_batch = num_data // batch_size * epoch
 for batch_index in range(num_batch):
     print('%s / %s' % (batch_index, num_batch))
     y, X = data_helper.get_batch_label_and_vector(data_loader, batch_size)
-    model.fit(X, y, batch_size=10)
+    model.fit(X, y, batch_size=batch_size)
 
 # 测试
 num_data = int(data_loader.num_data / 2)
