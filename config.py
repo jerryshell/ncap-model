6579be0881c33f804e5ca3d31d708b828803d8cb
Jerry <calljerryli@outlook.com>
2020-05-05 02:03:47 +0800

    修改注释

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-# 超参数
-feature1_number = 64  # 句子分成多少个词语，多余截断，不够补 0
-feature2_number = 300  # 每个词语的向量
+# 句子分成多少个词语，多余截断，不够补 0
+feature1_number = 64
+# 每个词语的向量
+feature2_number = 300
 
 # train
 train_model_filename = 'text_cnn_separable.2.h5'
