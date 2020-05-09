c7e41e45b3887aec063adf4bdc25673c888e723b
Jerry <calljerryli@outlook.com>
2020-05-09 17:09:50 +0800

    DataLoader 数据集切分优化

--- data_loader.py
+++ data_loader.py
@@ -8,29 +8,32 @@ class DataLoader:
     def __init__(self):
         # 加载数据
         data = pd.read_csv('./data/simplifyweibo_4_moods_cut.csv')
-        print(data.shape)
+        print('data.shape', data.shape)
         print(data.describe())
 
-        data = np.array(data)
+        # pandas 转 numpy
+        data = data.to_numpy()
         data_count = len(data)
 
         # 打乱数据集
         np.random.shuffle(data)
 
-        # 切分训练集
-        self.train_data_count = int(data_count * 0.8)
-        self.train_data = data[:self.train_data_count]
+        # 切分训练集、验证集、测试集 8:1:1
+        self.train_data, self.validation_data, self.test_data = np.vsplit(
+            data,
+            [int(data_count * 0.8), int(data_count * 0.9)]
+        )
         print('train_data.shape', self.train_data.shape)
-
-        # 切分验证集
-        self.validation_data_count = int(data_count * 0.1)
-        self.validation_data = data[self.train_data_count:self.train_data_count + self.validation_data_count]
         print('validation_data.shape', self.validation_data.shape)
-
-        # 切分测试集
-        self.test_data = data[self.train_data_count + self.validation_data_count:]
-        self.test_data_count = len(self.test_data)
         print('test_data.shape', self.test_data.shape)
+
+        # 记录训练集、验证集、测试集的大小
+        self.train_data_count = len(self.train_data)
+        self.validation_data_count = len(self.validation_data)
+        self.test_data_count = len(self.test_data)
+        print('train_data_count', self.train_data_count)
+        print('validation_data_count', self.validation_data_count)
+        print('test_data_count', self.test_data_count)
 
     def train_data_generator(self):
         return itertools.cycle(self.train_data)
