797cb38d59fe6e7f97582024916513370763be8c
Jerry <calljerryli@outlook.com>
2020-05-14 13:28:07 +0800

    cnn -> batch_normal -> relu -> max_pool

--- model_creator.py
+++ model_creator.py
@@ -21,40 +21,41 @@ def create_model_text_cnn(embedding_weights, embedding_trainable: bool):
     cnn1 = keras.layers.SeparableConv1D(
         filters=filters,
         kernel_size=kernel_sizes[0],
-        activation='relu',
         name='cnn1',
     )(embedding)
+    batch_normal1 = keras.layers.BatchNormalization(name='bn1')(cnn1)
+    relu1 = keras.layers.ReLU(name='relu1')(batch_normal1)
     max_pool1 = keras.layers.MaxPooling1D(
         name='max_pool1',
-    )(cnn1)
+    )(relu1)
 
     cnn2 = keras.layers.SeparableConv1D(
         filters=filters,
         kernel_size=kernel_sizes[1],
-        activation='relu',
         name='cnn2',
     )(embedding)
+    batch_normal2 = keras.layers.BatchNormalization(name='bn2')(cnn2)
+    relu2 = keras.layers.ReLU(name='relu2')(batch_normal2)
     max_pool2 = keras.layers.MaxPooling1D(
         name='max_pool2',
-    )(cnn2)
+    )(relu2)
 
     cnn3 = keras.layers.SeparableConv1D(
         filters=filters,
         kernel_size=kernel_sizes[2],
-        activation='relu',
         name='cnn3',
     )(embedding)
+    batch_normal3 = keras.layers.BatchNormalization(name='bn3')(cnn3)
+    relu3 = keras.layers.ReLU()(batch_normal3)
     max_pool3 = keras.layers.MaxPooling1D(
         name='max_pool3',
-    )(cnn3)
+    )(relu3)
 
     concatenate = keras.layers.Concatenate(axis=1)([max_pool1, max_pool2, max_pool3])
 
     flatten = keras.layers.Flatten()(concatenate)
 
-    batch_normal = keras.layers.BatchNormalization()(flatten)
-
-    outputs = keras.layers.Dense(units=2, activation='softmax')(batch_normal)
+    outputs = keras.layers.Dense(units=2, activation='softmax')(flatten)
 
     model = keras.Model(inputs=inputs, outputs=outputs)
     model.compile(
